633202aae1a892d77f694f1685cd26d7f918e110
Marc-Antoine Belanger <marcantoine.belanger@shopify.com>
2022-10-14 19:36:56 -0400

    change in lua installer ref to yue

--- lua/mab/lsp/lsp-installer.lua
+++ lua/mab/lsp/lsp-installer.lua
@@ -8,22 +8,22 @@ end
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
 	local opts = {
-		on_attach = require("yue.lsp.handlers").on_attach,
-		capabilities = require("yue.lsp.handlers").capabilities,
+		on_attach = require("mab.lsp.handlers").on_attach,
+		capabilities = require("mab.lsp.handlers").capabilities,
 	}
 
 	 if server.name == "jsonls" then
-	 	local jsonls_opts = require("yue.lsp.settings.jsonls")
+	 	local jsonls_opts = require("mab.lsp.settings.jsonls")
 	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
 	 end
 
 	 if server.name == "sumneko_lua" then
-	 	local sumneko_opts = require("yue.lsp.settings.sumneko_lua")
+	 	local sumneko_opts = require("mab.lsp.settings.sumneko_lua")
 	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
 	 end
 
 	 if server.name == "pyright" then
-	 	local pyright_opts = require("yue.lsp.settings.pyright")
+	 	local pyright_opts = require("mab.lsp.settings.pyright")
 	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
 	 end
 

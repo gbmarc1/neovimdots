e360f3e09267603e8a66da985da1e93e96453416
Marc-Antoine Belanger <marcantoine.belanger@shopify.com>
2022-10-14 18:59:47 -0400

    adding lsp

---
+++ lua/mab/lsp/init.lua
@@ -0,0 +1,9 @@
+-- entry point for lsp configs
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+    return
+end
+
+require("yue.lsp.lsp-installer")
+require("yue.lsp.handlers").setup()
+require("yue.lsp.null-ls")
